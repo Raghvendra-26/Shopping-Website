de71119ca662f38157f8f0d97503d016fa15b9ff
Raghvendra Singh <26rsr0104@gmail.com>
2024-05-03 17:10:26 +0530

    Ready to commit

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import './DescriptionBox.css'
+
+const DescriptionBox = ()=> {
+  return (
+
+    <div className="descriptionbox">
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">
+                Reviews (122)
+            </div>
+        </div>
+
+        <div className="descriptionbox-description">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur consequuntur, minus et enim expedita obcaecati rem adipisci ullam exercitationem sed, omnis quod, culpa magnam autem error nemo natus alias ipsa eligendi! Saepe distinctio earum deleniti fugit quidem, quam maxime aspernatur deserunt, optio vitae quaerat et necessitatibus ipsum inventore sit nesciunt.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, odit iusto? Excepturi labore hic molestiae deserunt aut maxime consequuntur sed. Perferendis culpa inventore praesentium tempore est fugiat alias, optio nostrum.
+            </p>
+        </div>
+    </div>
+)}
+
+export default DescriptionBox
